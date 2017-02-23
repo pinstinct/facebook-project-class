891dbf59184b334149a15aa8cbbcfaea0b186737
LimHM <gusals3407@gmail.com>
2017-02-24 00:07:25 +0900

    facebook login dialog redirect

--- django_app/member/urls.py
+++ django_app/member/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 app_name = 'member'
 urlpatterns = [
-    url('^login/$', views.login_fbv, name='login')
+    url(r'^login/$', views.login_fbv, name='login'),
+    url(r'^login/facebook/$', views.login_facebook, name='login_facebook'),
 ]
